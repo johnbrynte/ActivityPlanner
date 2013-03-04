76b7237f6bb78e22d1ca4e6551608da64d7d6fa9
adurenator <adurenator@gmail.com>
2013-03-04 14:15:25 +0100

    mvc

--- src/mvc/ChartController.java
+++ src/mvc/ChartController.java
@@ -1,12 +1,75 @@
 package mvc;
 
-public class ChartController
+import java.awt.event.MouseEvent;
+import javax.swing.event.MouseInputListener;
+
+public class ChartController implements MouseInputListener
 {
     private ChartView view;
+    private DnDController dnd;
+    private boolean communication = false;
     
-    public ChartController(ChartView v)
+    public ChartController(DnDController d, ChartView v)
     {
+        this.dnd  = d;
         this.view = v;
     }
     
+    void startComm()
+    {
+        communication = true;
+    }
+
+    void endComm()
+    {
+        communication = false;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e)
+    {
+        /*
+        boolean result = view. ...
+        if (result) {
+            if (communication) { dnd.notifyDropResult(true); }
+            else { performDrop }
+        }
+        else {
+           if (communication) { dnd.notifyDropResult(false); }
+           else { cancelDrop }
+        }
+        */
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e)
+    {
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e)
+    {
+    }
+
 }
--- src/mvc/ParkController.java
+++ src/mvc/ParkController.java
@@ -1,12 +1,67 @@
 package mvc;
 
-public class ParkController
+import java.awt.event.MouseEvent;
+import javax.swing.event.MouseInputListener;
+
+public class ParkController implements MouseInputListener
 {
     private ParkView view;
+    private DnDController dnd;
     
-    public ParkController(ParkView v)
+    public ParkController(DnDController d, ParkView v)
     {
-        this.view = v;
+        this.dnd    = d;
+        this.view   = v;
+    }
+    
+    void setDropResult(boolean result)
+    {
+        if (result) {
+            // delete activity from park
+        }
+        else {
+            // keep activity in park
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e)
+    {
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e)
+    {
+        // detect whether the mouse is on the upper view
+        if (e.getY() < 0) { dnd.transferDraggingEvent(e, DnDController.RELEASE); }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e)
+    {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e)
+    {
+        // detect whether the mouse is on the upper view
+        if (e.getY() < 0) { dnd.transferDraggingEvent(e, DnDController.DRAG); }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e)
+    {
     }
     
 }
